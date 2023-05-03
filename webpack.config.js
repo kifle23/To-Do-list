2ffd0cc93a70ce05abe23bdab7349f293e5a2d2b
kifle23 <68891198+kifle23@users.noreply.github.com>
2023-05-03 16:44:29 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: {
     index: './src/index.js',
   },
