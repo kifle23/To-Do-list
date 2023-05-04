3c8985ae722abc316f9c00eb0381d8a481fc1bfa
kifle23 <68891198+kifle23@users.noreply.github.com>
2023-05-04 14:21:49 +0300

    Add interaction

--- src/modules/dragDrop.js
+++ src/modules/dragDrop.js
@@ -1,42 +1,42 @@
 export default () => {
-  const task = document.querySelector(".list-item");
-  task.addEventListener("dragstart", dragStart);
+  const task = document.querySelector('.list-item');
 
-  function dragStart(e) {
-    e.dataTransfer.setData("text/plain", e.target.id);
+  const dragStart = (e) => {
+    e.dataTransfer.setData('text/plain', e.target.id);
     setTimeout(() => {
-      e.target.classList.add("hide");
+      e.target.classList.add('hide');
     }, 0);
-  }
+  };
 
-  const lists = document.querySelectorAll(".to-do-list");
-  lists.forEach((list) => {
-    list.addEventListener("dragenter", dragEnter);
-    list.addEventListener("dragover", dragOver);
-    list.addEventListener("dragleave", dragLeave);
-    list.addEventListener("drop", drop);
-  });
-
-  function dragEnter(e) {
+  const dragEnter = (e) => {
     e.preventDefault();
-    e.target.classList.add("drag-over");
-  }
+    e.target.classList.add('drag-over');
+  };
 
-  function dragOver(e) {
+  const dragOver = (e) => {
     e.preventDefault();
-    e.target.classList.add("drag-over");
-  }
+    e.target.classList.add('drag-over');
+  };
 
-  function dragLeave(e) {
-    e.target.classList.remove("drag-over");
-  }
+  const dragLeave = (e) => {
+    e.target.classList.remove('drag-over');
+  };
 
-  function drop(e) {
-    e.target.classList.remove("drag-over");
-    const id = e.dataTransfer.getData("text/plain");
+  const drop = (e) => {
+    e.target.classList.remove('drag-over');
+    const id = e.dataTransfer.getData('text/plain');
     const draggable = document.getElementById(id);
-    const list = e.target.closest(".to-do-list");
+    const list = e.target.closest('.to-do-list');
     list.appendChild(draggable);
-    draggable.classList.remove("hide");
-  }
+    draggable.classList.remove('hide');
+  };
+
+  task.addEventListener('dragstart', dragStart);
+  const lists = document.querySelectorAll('.to-do-list');
+  lists.forEach((list) => {
+    list.addEventListener('dragenter', dragEnter);
+    list.addEventListener('dragover', dragOver);
+    list.addEventListener('dragleave', dragLeave);
+    list.addEventListener('drop', drop);
+  });
 };
